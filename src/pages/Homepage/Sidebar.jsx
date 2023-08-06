13f71306c77f571d6bd1a2fa2c0237c38bd51ce1
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-08-06 13:51:10 -0400

    Identify Redundancies and optimized the Sidebar

--- src/pages/Homepage/Sidebar.jsx
+++ src/pages/Homepage/Sidebar.jsx
@@ -11,6 +11,21 @@ const Sidebar = () => {
   const linkedinContainer = useRef(null);
   const resumeContainer = useRef(null);
 
+  const handleExternalLinkClick = (link) => {
+    window.open(link, "_blank");
+  };
+
+  const sidebarItems = [
+    { ref: homeContainer, path: '/homepage.json', handler: () => handleScroll('home'), title: 'Home' },
+    { ref: aboutMeContainer, path: '/aboutme.json', handler: () => handleScroll('about-me'), title: 'About Me' },
+    { ref: projectsContainer, path: '/projects.json', handler: () => handleScroll('projects'), title: 'Projects' },
+    { ref: contactContainer, path: '/projects.json', handler: () => handleScroll('contact'), title: 'Contact' },
+    { ref: githubContainer, path: '/github.json', handler: () => handleExternalLinkClick('https://github.com/fzinnah17'), title: 'Github' },
+    { ref: linkedinContainer, path: '/linkedin.json', handler: () => handleExternalLinkClick('https://www.linkedin.com/in/farnaz-zinnah/'), title: 'LinkedIn' },
+    { ref: resumeContainer, path: '/resume.json', handler: () => handleExternalLinkClick('https://drive.google.com/file/d/1Ob2PkmUj22DiadrF4dFDjvHCkcD8Eoe9/view?usp=sharing'), title: 'Resume' },
+  ];
+
+
   const handleScroll = (id) => {
     const element = document.getElementById(id);
     if (element) {
@@ -19,110 +34,21 @@ const Sidebar = () => {
   };
   console.log("Sidebar rendered");
 
-
   useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: homeContainer.current, // the dom element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/homepage.json', // the path to the animation json
+    const animations = sidebarItems.map(item => {
+      return lottie.loadAnimation({
+        container: item.ref.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: item.path,
+      });
     });
+
     return () => {
-      animation.destroy();
+      animations.forEach(animation => animation.destroy());
     };
   }, []);
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: aboutMeContainer.current, // the dom element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/aboutme.json', // the path to the animation json
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: projectsContainer.current, // the dom element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/projects.json', // the path to the animation json
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: contactContainer.current, // the dom element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/projects.json', // the path to the animation json
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: githubContainer.current, // the DOM element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/github.json', // update with your path
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  const handleGithubClick = () => {
-    window.open("https://github.com/fzinnah17", "_blank");
-  }
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: linkedinContainer.current, // the DOM element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/linkedin.json', // update with your path
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  const handleLinkedinClick = () => {
-    window.open("https://www.linkedin.com/in/farnaz-zinnah/", "_blank");
-  }
-
-
-  useEffect(() => {
-    const animation = lottie.loadAnimation({
-      container: resumeContainer.current, // the DOM element that will contain the animation
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: '/resume.json', // update with your path
-    });
-    return () => {
-      animation.destroy();
-    };
-  }, []);
-
-  const handleResumeClick = () => {
-    window.open("https://drive.google.com/file/d/1Ob2PkmUj22DiadrF4dFDjvHCkcD8Eoe9/view?usp=sharing", "_blank");
-  }
 
   return (
     <div className="sidebar">
@@ -130,43 +56,60 @@ const Sidebar = () => {
         <img src="/images/fz-low-resolution-color-logo-transparent.png" alt="Logo" className="logo" />
       </div>
 
-      <div className="sidebar-item" onClick={() => handleScroll("home")} title="Home">
-        <div className="lottie-container" ref={homeContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">Home</span>
-      </div>
-
-      <div className="sidebar-item" onClick={() => handleScroll("about-me")} title="About Me">
-        <div className="lottie-container" ref={aboutMeContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">About Me</span>
-      </div>
-
-      <div className="sidebar-item" onClick={() => handleScroll("projects")} title="Projects">
-        <div className="lottie-container" ref={projectsContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">Projects</span>
-      </div>
-
-      <div className="sidebar-item" onClick={() => handleScroll("contact-me")} title="Contact">
-        <div className="lottie-container" ref={contactContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">Contact</span>
-      </div>
-
-      <div className="sidebar-item" onClick={handleGithubClick}>
-        <div className="lottie-container" ref={githubContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">Github</span>
-      </div>
-
-      <div className="sidebar-item" onClick={handleLinkedinClick}>
-        <div className="lottie-container" ref={linkedinContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">LinkedIn</span>
-      </div>
-
-      <div className="sidebar-item" onClick={handleResumeClick}>
-        <div className="lottie-container" ref={resumeContainer} style={{ width: 50, height: 50 }}></div>
-        <span className="hover-text">Resume</span>
-      </div>
+      {sidebarItems.map((item, index) => (
+        <div key={index} className="sidebar-item" onClick={item.handler} title={item.title}>
+          <div className="lottie-container" ref={item.ref} style={{ width: 50, height: 50 }}></div>
+          <span className="hover-text">{item.title}</span>
+        </div>
+      ))}
 
     </div>
   );
 };
 
 export default Sidebar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// General Steps for React Component Optimization:
+
+// 1. **Identify Redundancies**: Before optimizing, recognize the patterns and repetitive parts in your code. In the provided code, the repeated sections were the Lottie animation blocks and the sidebar items.
+
+// 2. **Consolidate Data into Arrays or Objects**: Instead of managing each item separately, consolidate similar items into an array or object. In the provided example, we used an array (`sidebarItems`) to represent the different sidebar elements.
+
+// 3. **Use References**: 
+//     - When working with direct DOM manipulations or third-party libraries in React, you often need to use refs.
+//     - Define the refs using `useRef(null)`.
+//     - Associate each ref with an element in the array or object you created.
+
+// 4. **Optimize Event Handlers**: 
+//     - If multiple items share similar functionality (like opening a link), create a single event handler function that can be parameterized.
+//     - For instance, instead of separate functions to open GitHub, LinkedIn, etc., we created a generalized `handleExternalLinkClick(link)`.
+
+// 5. **Optimize Lifecycles**: 
+//     - If you're performing similar side effects (like loading animations) for multiple items, utilize React's lifecycle methods (e.g., `useEffect`) to handle them collectively.
+//     - In the example, we loaded all Lottie animations in a single `useEffect` using the map function.
+
+// 6. **Use Map for Rendering**:
+//     - Use the `map` function to loop through the array or object to render each item. This helps in reducing JSX duplication.
+//     - Ensure you provide a unique `key` prop for each item when mapping.
+
+// 7. **Test**: Always test the optimized component to ensure that it works as expected after the changes.
+
+// 8. **Stay Updated**: React and its ecosystem are continually evolving. Best practices and optimizations might change over time. It's essential to stay updated with the latest developments and changes.
+
+// Remember, these are general steps and might need adjustments based on the specific use case or component you're working with. The key is to always look for patterns and think about how you can make your code more DRY (Don't Repeat Yourself). This not only makes the code more maintainable but can also lead to performance improvements.
