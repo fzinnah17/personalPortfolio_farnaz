8b832aa5136f0cd839079679ce3f83b4c9589c6d
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-08-14 14:22:56 -0400

    basic css of projects section added

--- src/pages/Homepage/Projects.jsx
+++ src/pages/Homepage/Projects.jsx
@@ -1,76 +1,67 @@
 import React from "react";
-import { Text, Button, Img } from "components";
+import { FaJsSquare, FaPython, FaReact, FaJava, FaNode, FaHtml5, FaCss3, FaGithub } from "react-icons/fa";
 import "./Projects.css";
 
 const Projects = () => {
-  return (
-    <div className="projects">
-      <div className="projects-container">
-        <div className="project-card">
-          <Button className="cut-button" shape="icbRoundedBorder10" size="smIcn" variant="icbFillRed401">
-            <Img src="images/img_group683.svg" alt="group683" className="group683-icon" />
-          </Button>
-          <div className="project-details">
-            <Text className="project-text" variant="body3">
-              I am very satisfied with the work on the Nexus team.
-              good service, satisfying work results and can also
-              increase my business turnover. Thank you so much to
-              the Nexus team.
-            </Text>
-            <div className="project-info">
-              <Img src="images/img_rectangle52.png" alt="RectangleFiftyTwo" className="project-image" />
-              <div className="project-info-details">
-                <Text className="project-name" variant="body2">John Richard</Text>
-                <Text className="project-role" variant="body5">Businessman</Text>
-              </div>
-            </div>
-          </div>
-        </div>
+  const technologies = 
+  [
+    { Icon: FaJsSquare, text: "Javascript ES6+" },
+    { Icon: FaPython, text: "Python" },
+    { Icon: FaReact, text: "React.js" },
+    { Icon: FaJava, text: "Java" },
+    { Icon: FaNode, text: "Node.js" },
+    { Icon: FaHtml5, text: "HTML" },
+    { Icon: FaCss3, text: "CSS" },
+  ]
 
+const project = [
+  {
+    title: "Portfolio",
+    description: "Short description of the project.",
+    tech: [FaReact, FaHtml5, FaCss3],
+    github: "github_link",
+  },
+  {
+    title: "Portfolio",
+    description: "Short description of the project.",
+    tech: [FaReact, FaHtml5, FaCss3],
+    github: "github_link",
+  },
+  {
+    title: "Portfolio",
+    description: "Short description of the project.",
+    tech: [FaReact, FaHtml5, FaCss3],
+    github: "github_link",
+  },
+  {
+    title: "Portfolio",
+    description: "Short description of the project.",
+    tech: [FaReact, FaHtml5, FaCss3],
+    github: "github_link",
+  },
+];
+return (
+  <div className="projects">
+    <div className="projects-container">
+      {project.map(project => (
         <div className="project-card">
-          <Button className="cut-button" shape="icbRoundedBorder10" size="smIcn" variant="icbFillRed401">
-            <Img src="images/img_group684.svg" alt="group684" className="group684-icon" />
-          </Button>
-          <div className="project-details">
-            <Text className="project-text" variant="body3">
-              since branding with the oracle team i feel my business
-              has a very fast and good development, everything also
-              always goes through a process of discussion that is
-              really valid
-            </Text>
-            <div className="project-info">
-              <Img src="images/img_rectangle52_49X49.png" alt="RectangleFiftyTwo One" className="project-image" />
-              <div className="project-info-details">
-                <Text className="project-name" variant="body2">Andrew Smith</Text>
-                <Text className="project-role" variant="body5">Founder Ulala</Text>
-              </div>
-            </div>
+          <img src="path_to_project_image" alt={project.title} className="project-image" />
+          <h3 className="project-title">{project.title}</h3>
+          <p className="project-description">{project.description}</p>
+          <div className="tech-icons">
+            {project.tech.map(TechIcon => (
+              <TechIcon className="tech-icon" />
+            ))}
           </div>
+          <a href={project.github} target="_blank" rel="noopener noreferrer" className="github-link">
+            <FaGithub />
+          </a>
         </div>
-
-        <div className="project-card">
-          <Button className="cut-button" shape="icbRoundedBorder10" size="smIcn" variant="icbFillRed401">
-            <Img src="images/img_group684.svg" alt="group684" className="group684-icon" />
-          </Button>
-          <div className="project-details">
-            <Text className="project-text" variant="body3">
-              I can say, this team is indeed extraordinarily
-              perfect. I will do a lot of teamwork with this team
-              next time. I do not hesitate to cooperate because the
-              results are very satisfying
-            </Text>
-            <div className="project-info">
-              <Img src="images/img_rectangle52_49X49.png" alt="RectangleFiftyTwo Two" className="project-image" />
-              <div className="project-info-details">
-                <Text className="project-name" variant="body2">Steve John</Text>
-                <Text className="project-role" variant="body5">CEO rumahku.ox</Text>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
-  );
+  </div>
+);
+  
 };
 
 export default Projects;
