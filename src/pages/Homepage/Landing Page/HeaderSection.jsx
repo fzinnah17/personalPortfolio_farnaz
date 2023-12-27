ed7b9df99e919ad611e90120ca18fe496287af39
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-12-27 17:43:59 -0500

    Update HeaderSection styling and remove commented code

--- src/pages/Homepage/Landing Page/HeaderSection.jsx
+++ src/pages/Homepage/Landing Page/HeaderSection.jsx
@@ -1,3 +1,39 @@
+// import React, { useEffect, useState } from "react";
+// import "./HeaderSection.css";
+
+// export const HeaderSection = () => {
+    // const [greetingsIndex, setGreetingsIndex] = useState(0);
+    // const greetings = ["world !", "fellows !", "everyone !", "recruiters !"];
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setGreetingsIndex((prevIndex) => (prevIndex + 1) % greetings.length);
+    //     }, 1500);  // Switch greeting every 1.5 seconds
+
+    //     return () => clearInterval(interval);  // Cleanup the interval on component unmount
+    // }, []);
+
+//     return (
+//         <div className="header-label">
+//             <p className="header-greeting">
+//                 <div className="name-content">
+//                     <div className="namecontent__container">
+//                         <p className="namecontent__container__text">Hello,</p>
+//                         <ul className="namecontent__container__list">
+//                             <li className="namecontent__container__list__item">
+//                                 {greetings[greetingsIndex]}
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//                 <span className="span">I am</span>
+//                 <span className="header-text-wrapper-2"> Farnaz<br /></span>
+//                 <span className="header-text-wrapper-3">I love to dive deep into frontend magic and backend logic.</span>
+//             </p>
+//         </div>
+//     );
+// };
+
 import React from "react";
 import "./HeaderSection.css";
 
